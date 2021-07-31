5e880a25c6c0dd0c3661f7a5303d19ba45805470
white-jang <galaxy__12@naver.com>
2021-07-31 12:16:53 +0900

    template mapping 오류

---
+++ demo/src/main/java/controller/HelloController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello-mvc")
+    public String helloTemplate(String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody
+    public String helloString(String name) {return String.format("Hello %s");}
+
+    @GetMapping("/hello-map")
+    @ResponseBody
+    public Map<String, Object> helloMap(String name) {
+        Map<String, Object> m = new HashMap<>();
+        m.put("name", name);
+        return m;
+    }
+
+//    @GetMapping("/hello-object")
+//    @ResponseBody
+//    public Hello helloObject(String name) {
+//        Hello h = new Hello(name);
+//        return h;
+//    }
+}
